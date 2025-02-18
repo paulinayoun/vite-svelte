d26ddae15edffc73b366b5f37d735583226d1688
HyeJeong Yoon <143067143+paulinayoun@users.noreply.github.com>
2025-02-18 14:21:38 +0900

    1. bar chart 되돌리기

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "carbon-components-svelte/css/all.css";
   import Modal from './Components/Modal.svelte';
+  import Barchart from "./Components/BarChart.svelte";
   import LineChart from "./Components/LineChart.svelte";
 
   let theme = "g90"; // "white" | "g10" | "g80" | "g90" | "g100"
@@ -14,6 +15,7 @@
   </div>
 
   <div class="chart">
+    <Barchart />
     <LineChart />
   </div>
 </main>
