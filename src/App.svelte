8e89708f730cbd48b81544f0dc77199cfa1fb126
HyeJeong Yoon <143067143+paulinayoun@users.noreply.github.com>
2025-02-18 14:11:58 +0900

    1.bar chart만 남기기

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import "carbon-components-svelte/css/all.css";
   import Modal from './Components/Modal.svelte';
   import Barchart from "./Components/BarChart.svelte";
-  import LineChart from "./Components/LineChart.svelte";
 
   let theme = "g90"; // "white" | "g10" | "g80" | "g90" | "g100"
 
@@ -16,7 +15,6 @@
 
   <div class="chart">
     <Barchart />
-    <LineChart />
   </div>
 </main>
 
